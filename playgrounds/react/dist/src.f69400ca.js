cd2aaaa5626843cc5e90793a7bd32b58cff69ddb
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-09-01 16:33:36 +0300

    using lerna to watch and build files in the workspace

--- playgrounds/react/dist/src.f69400ca.js
+++ playgrounds/react/dist/src.f69400ca.js
@@ -29720,7 +29720,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50734" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59241" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
