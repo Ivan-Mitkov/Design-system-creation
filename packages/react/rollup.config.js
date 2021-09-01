63f391a19c53993bb97cee0bd23a74e2c978c551
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-09-01 15:17:45 +0300

    add rollup to build react

---
+++ packages/react/rollup.config.js
@@ -0,0 +1,19 @@
+import Ts from "rollup-plugin-typescript2";
+//https://rollupjs.org/guide/en/#configuration-files
+// in package.json "build":"rollup -c" to use this configuration
+export default {
+  input: [
+    "src/index.ts",
+    //user will be able to import just this files
+    "src/atoms/Button/index.ts",
+  ],
+  output: {
+    dir: "lib",
+    format: "esm",
+    sourcemap: true,
+  },
+  plugins: [Ts()],
+  //preserve structure of src folder
+  preserveModules: true,
+  external:['react']
+};
