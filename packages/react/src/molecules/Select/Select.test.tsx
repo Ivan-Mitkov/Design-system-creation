c7bfbc962796d2bc77e00a01d391ccca11dbbc21
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-09-13 15:08:21 +0300

    more tests

--- packages/react/src/molecules/Select/Select.test.tsx
+++ packages/react/src/molecules/Select/Select.test.tsx
@@ -43,10 +43,50 @@ test("renders options using custom renderOption method if passed as props", () =
   expect(getAllByTestId("customRenderOption")).toHaveLength(options.length);
 });
 
-test("calls the onOptionSelected prop with the selected option and its index if passed ", () => {});
+test("calls the onOptionSelected prop with the selected option and its index if passed ", () => {
+  const onOptioSelected = jest.fn();
+  const { getAllByRole, getByTestId } = render(
+    <Select options={options} onOptionSelected={onOptioSelected} />
+  );
+  fireEvent.click(getByTestId("DseSelectButton"));
+  fireEvent.click(getAllByRole("menuitemradio")[0]);
+  // debug()
+  expect(onOptioSelected).toHaveBeenCalledWith(options[0], 0);
+});
 
-test("the button label changes to the selected option label ", () => {});
+test("the button label changes to the selected option label ", () => {
+  const { getAllByRole, getByTestId, debug } = render(
+    <Select options={options} />
+  );
+  fireEvent.click(getByTestId("DseSelectButton"));
+  fireEvent.click(getAllByRole("menuitemradio")[0]);
+  // debug();
+  expect(getByTestId("DseSelectButton")).toHaveTextContent(options[0].label);
+});
 
-test("snapshot of the selected option state ", () => {});
-test("snapshot of the base state ", () => {});
-test("snapshot of the options menu open state ", () => {});
+test("snapshot of the selected option state ", () => {
+  const { getAllByRole, getByTestId, asFragment } = render(
+    <Select options={options} />
+  );
+  fireEvent.click(getByTestId("DseSelectButton"));
+  fireEvent.click(getAllByRole("menuitemradio")[0]);
+  expect(asFragment()).toMatchSnapshot();
+});
+test("snapshot of the base state ", () => {
+  const { asFragment } = render(<Select options={options} />);
+
+  expect(asFragment()).toMatchSnapshot();
+});
+test("snapshot of the options menu open state ", () => {
+  const { getByTestId, asFragment } = render(<Select options={options} />);
+  fireEvent.click(getByTestId("DseSelectButton"));
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test("can customize select label ", () => {
+  const { getByText } = render(
+    <Select options={options} label="This is a custom label" />
+  );
+
+  expect(getByText(/This is a custom label/)).toBeInTheDocument();
+});
